12aca7e42301352bced1cb6912ae25afd5863d64
oguyon <oliv.guyon@gmail.com>
2017-12-28 22:02:39 -1000

    msync

--- image_format.c
+++ image_format.c
@@ -257,7 +257,12 @@ int_fast8_t IMAGE_FORMAT_loadCR2toFITSRGB_cli()
 void __attribute__ ((constructor)) libinit_image_format()
 {
 	init_image_format();
-//	printf(" ...... Loading module %s\n", __FILE__);
+
+	if(data.progStatus>0)
+	{
+		printf("  Found unloaded shared object in ./libs/ -> LOADING module %s\n", __FILE__);
+		fflush(stdout);
+	}	
 }
 
 
